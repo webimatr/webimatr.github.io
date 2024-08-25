31746b1b3012ab55682dc3e39b5062b1b774fd32
avery-jpg <jxstmeliving@gmail.com>
2024-08-24 17:05:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Webimatr site is ready!');
 });
+
+function copyCode() {
+    const codeBlock = document.getElementById('code-block');
+    const range = document.createRange();
+    range.selectNode(codeBlock);
+    window.getSelection().removeAllRanges(); // Clear any existing selections
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges(); // Clear the selection after copying
+    alert('Code copied to clipboard!');
+}
